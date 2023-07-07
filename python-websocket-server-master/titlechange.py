0c3b6b4c52e25949c35a9262e0c83e985438798d
magical <sun920122@gmail.com>
2023-07-07 17:11:10 +0800

    fix: filename is not const

--- python-websocket-server-master/titlechange.py
+++ python-websocket-server-master/titlechange.py
@@ -8,24 +8,26 @@ import shutil
 import linecache
 #from titleReader import *
 
-def title_change(save_data, data):
+def title_change(save_data, data, file_name):
     new_file = []
-    with open("config.h", 'r+') as f:
-        new_file.append(f.readline())
+    with open(file_name, 'r+') as f:
+        new_file = f.read().splitlines()
 
     for title in data:
         line_num = save_data[title]['line']
         line_content = new_file[line_num]
-        change = data[title]['value']
+        new_value = data[title]
 
-        if change != save_data[title]['value']:
-            if type(change) != bool:
-                new_file[line_num] = line_content.lstripe(' ').replace(save_data[title]['value'], data[title]['value'])
+        if new_value != save_data[title]['value']:
+            if type(new_value) != bool:
+                new_file[line_num] = line_content.lstrip(' ').replace(save_data[title]['value'], data[title])
+                save_data[title]['value'] = data[title]
             else:
-                if change == False:
-                    new_file[line_num] = '//'+line_content.lstripe(' ')
+                if new_value:
+                    new_file[line_num] = line_content.lstrip(' /')
                 else:
-                    new_file[line_num] =line_content.lstripe(' /')
+                    new_file[line_num] = '//' + line_content.lstrip(' ')
+                save_data[title]['value'] = data[title]
         else:
             pass
     
