e5a342a8f58972e8477c9d775ebe3e3d4c7b166a
magical <sun920122@gmail.com>
2023-07-13 18:22:08 +0800

    fix: header fixed

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,38 +1,10 @@
 <template>
   <CHeader position="sticky" class="mb-4">
     <CContainer fluid>
-      <CHeaderToggler class="ps-1">
+      <CHeaderBrand class="mx-auto d-lg-none" to="#">Config Modifier</CHeaderBrand>
+      <CHeaderToggler class="ps-1" @click="$store.commit('toggleSidebar')">
       </CHeaderToggler>
-      <CHeaderBrand class="mx-auto d-lg-none" to="/">
-      </CHeaderBrand>
-      <CHeaderNav class="d-none d-md-flex me-auto">
-        <CNavItem>
-          <CNavLink href="/dashboard"> Dashboard </CNavLink>
-        </CNavItem>
-        <CNavItem>
-          <CNavLink href="#">Users</CNavLink>
-        </CNavItem>
-        <CNavItem>
-          <CNavLink href="#">Settings</CNavLink>
-        </CNavItem>
-      </CHeaderNav>
-      <CHeaderNav>
-        <CNavItem>
-          <CNavLink href="#">
-          </CNavLink>
-        </CNavItem>
-        <CNavItem>
-          <CNavLink href="#">
-          </CNavLink>
-        </CNavItem>
-        <CNavItem>
-          <CNavLink href="#">
-          </CNavLink>
-        </CNavItem>
-      </CHeaderNav>
     </CContainer>
-    <CHeaderDivider />
-    <CContainer fluid> </CContainer>
   </CHeader>
 </template>
 
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -15,22 +15,13 @@ const router = createRouter({
           name: 'ConfigHeader',
           component: () => import('@/views/ConfigHeader.vue'),
         },
+        {
+          path: 'header',
+          name: 'Header',
+          component: () => import('@/views/Header.vue'),
+        }
       ]
     },
-
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
   ]
 })
 
