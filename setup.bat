7326de49059c585c30f9fa171821376aeed6d816
magical <sun920122@gmail.com>
2023-07-14 13:59:29 +0800

    7/14

--- setup.bat
+++ setup.bat
@@ -1,19 +1,21 @@
-REM Check if python is installed
+@ECHO OFF
+
+@REM Check if python is installed
 @python --version
-IF %ERRORLEVEL% NEQ 0 (
+@IF %ERRORLEVEL% NEQ 0 (
     ECHO "Python is not installed. Please install python and try again."
     EXIT /B 1
 ) ELSE (
     ECHO "Python is installed."
 )
 
-REM Check if npm is installed
-@npm --version
-IF %ERRORLEVEL% NEQ 0 (
-    ECHO "npm is not installed. Please install npm and try again."
+@REM Check if node is installed
+@node --version
+@IF %ERRORLEVEL% NEQ 0 (
+    ECHO "Node is not installed. Please install node and try again."
     EXIT /B 1
 ) ELSE (
-    ECHO "npm is installed."
+    ECHO "Node is installed."
 )
 
 ECHO "Building environment..."
@@ -26,5 +28,4 @@ CD ..
 REM frontend environment
 CD .\frontend
 npm install
-npm run build
 CD ..
